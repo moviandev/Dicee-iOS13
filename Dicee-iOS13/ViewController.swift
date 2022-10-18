157a13938f1da1eec213ba881a60723632e16b51
Matheus Viana <matheus_o_viana@hotmail.com>
2022-10-18 16:26:07 -0300

    ádded functionality to our app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //IBOutlet allows to reference UI Elements
+    @IBOutlet weak var diceImageView1: UIImageView!;
+    @IBOutlet weak var diceImageView2: UIImageView!;
+    
+    var leftDiceNumber = 1;
+    var rightDiceNumber = 5;
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = [UIImage(named: "DiceOne"),
+         UIImage(named: "DiceTwo"),
+         UIImage(named: "DiceThree"),
+         UIImage(named: "DiceFour"),
+         UIImage(named: "DiceFive"),
+         UIImage(named: "DiceSix")][leftDiceNumber];
+        
+        diceImageView2.image = [UIImage(named: "DiceOne"),
+         UIImage(named: "DiceTwo"),
+         UIImage(named: "DiceThree"),
+         UIImage(named: "DiceFour"),
+         UIImage(named: "DiceFive"),
+         UIImage(named: "DiceSix")][rightDiceNumber];
+        
+        leftDiceNumber = leftDiceNumber + 1;
+        rightDiceNumber = rightDiceNumber - 1;
+    }
 }
 
